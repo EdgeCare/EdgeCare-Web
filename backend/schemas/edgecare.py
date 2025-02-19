75c038d756cd425eaf68d81f4070f288103f10e9
Thejan Weerasekara <92569392+ThejanB@users.noreply.github.com>
2025-02-19 11:36:04 +0530

    Middleware and schema updated

--- backend/schemas/edgecare.py
+++ backend/schemas/edgecare.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from datetime import datetime
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 class Request(BaseModel):
-    data: str
+    text: str
 
 class Response(BaseModel):
     response: str
+    results: List[Dict[str, str]] 
